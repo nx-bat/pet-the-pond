145e0f7365e9d4bd409d920a39833eaad74ca555
Nix Krystik <nix@archwing.dev>
2026-09-13 20:42:50 +0800

    it's uhh, fixed.

--- src/events/message/messageReactionAdd.ts
+++ src/events/message/messageReactionAdd.ts
@@ -7,14 +7,19 @@ class MessageReactionAddEvent extends Event<CommandClient> {
   event: string = 'messageReactionAdd' as const;
 
   async handle(context: CommandClient<any, any>, message: Message, emoji: Constants.APIEmoji, member: Member) {
-    if (!message.guildID || emoji.id != '1547870117080342548' || member.id == message.author.id) return;
+    try {
+      const _message = await context.getMessage(message.channel.id, message.id);
+      if (!_message.guildID || emoji.id != '1547870117080342548' || member.id == _message.author.id) return;
 
-    await database`
-      INSERT INTO points AS p (user_id, guild_id, points)
-        VALUES (${message.author.id}, ${member.guild.id}, 1)
-      ON CONFLICT (user_id, guild_id)
-        DO UPDATE SET points = p.points + 1
-    `;
+      await database`
+        INSERT INTO points AS p (user_id, guild_id, points)
+          VALUES (${_message.author.id}, ${_message.guildID}, 1)
+        ON CONFLICT (user_id, guild_id)
+          DO UPDATE SET points = p.points + 1
+      `;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
